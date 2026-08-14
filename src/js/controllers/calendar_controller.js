2272267eec816cdbc937ccc0316a6a5205d3aa5e
junker <junker@jonas-heitz.eu>
2026-08-14 13:08:12 +0200

    fixed api path, for when root dir is adjusted

--- src/js/controllers/calendar_controller.js
+++ src/js/controllers/calendar_controller.js
@@ -3,7 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     async connect() {
         console.log("connected", this.element)
-        const weather = await (await fetch("/public/api/weather.php")).json()
-        console.log("weather", weather)
+        console.log("fetching now")
+        const weather = await fetch("/api/weather.php").then(async res => {
+            console.log(await res.json())
+        })
     }
 }
