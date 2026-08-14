ba6c2b62d06e0402747fb2b135e7a76afe070eb8
junker <junker@jonas-heitz.eu>
2026-08-14 13:09:21 +0200

    mini thingy

--- src/js/controllers/calendar_controller.js
+++ src/js/controllers/calendar_controller.js
@@ -3,8 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     async connect() {
         console.log("connected", this.element)
-        console.log("fetching now")
-        const weather = await fetch("/api/weather.php").then(async res => {
+        await fetch("/api/weather.php").then(async res => {
             console.log(await res.json())
         })
     }
